aa2c03a9bcc4cc29ba7d334923b7e10ac030feb7
SENDA TOMOKO <sendatomoko@Tomos-MacBook-Air-2024.local>
2024-05-31 14:12:57 +0900

    レビューの修正をしました

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe "#full_title" do
     it "商品詳細ページ遷移時に正常にタイトルを取得できること" do
-      page_title = "Sample Product"
-      expect(helper.full_title(page_title)).to eq("Sample Product - BIGBAG Store")
+      expect(helper.full_title("Sample Product")).to eq("Sample Product - BIGBAG Store")
     end
 
     it "引数を設定しない場合にデフォルトのタイトルを取得できること" do
       expect(helper.full_title('')).to eq("BIGBAG Store")
+    end
+
+    it "引数がnilの場合にデフォルトのタイトルを取得できること" do
+      expect(helper.full_title(nil)).to eq("BIGBAG Store")
     end
   end
 end
